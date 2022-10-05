8607b7df5d8870ea0061a97154cbbbcdac0b2aab
D-Zakharov <103581622+D-Zakharov@users.noreply.github.com>
2022-10-05 09:50:35 +0300

    Update 3-StoredProc.sql

--- sql/3-StoredProc.sql
+++ sql/3-StoredProc.sql
@@ -15,14 +15,13 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
 	SELECT pv.ID, pr.[Name], pv.[Name], pv.[Length], pv.Width, pv.Height
 	from TestDb.[Prd].[Product] pr
 	left join TestDb.[Prd].[ProductVersion] pv on pv.ProductID = pr.ID
 	where 
 	 (@ProductPartName is null or pr.[Name] like '%' + @ProductPartName + '%') and
 	 (@ProductVersionPartName is null or pv.[Name] like '%' + @ProductVersionPartName + '%') and
-	 (@MinSize is null or @MinSize <= pv.Height * pv.[Length] * pv.Width) and
-	 (@MinSize is null or @MaxSize >= pv.Height * pv.[Length] * pv.Width)
+	 (@MinSize is null or @MinSize <= pv.TotalSize) and
+	 (@MinSize is null or @MaxSize >= pv.TotalSize)
 END
 GO
